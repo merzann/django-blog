74d3d1b3dc879e3d517a1521b7534e305b606d35
merzann <annisch78@gmail.com>
2024-12-13 21:35:34 +0000

    feat: add POST model to models.py, add CSRF_TRUSTED_ORIGIN to settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -19,14 +19,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9pw&34#!$_&)a&*y1_ka%*^#07yeq3b9maj$e)b8+xf!il#*$+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-merzann-djangoblog-p8354yz3bl1.ws-eu117.gitpod.io','.herokuapp.com']
@@ -88,6 +82,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-merzann-djangoblog-p8354yz3bl1.ws-eu117.gitpod.io"
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
