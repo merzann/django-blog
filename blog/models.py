6b4ed38e99567bdd3814e33de37288f9d43ae4e4
merzann <annisch78@gmail.com>
2024-12-14 01:48:03 +0000

    repeated commit

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    content = models.TextField(null=True, blank=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
