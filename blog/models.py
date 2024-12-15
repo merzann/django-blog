49aa93e93e3fd62bfe9c4d6c3df47d869c44feb1
merzann <annisch78@gmail.com>
2024-12-15 02:44:56 +0000

    feat: addfixtures and posts.json file, load posts.json into database

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
