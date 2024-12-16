11be1f9ebf15ea226aaa3caff8b4f4d541c85e6d
merzann <annisch78@gmail.com>
2024-12-16 02:34:48 +0000

    feat: update post_detail function in views.py to allow users to post a comment for a vlog post, add functionality to display a success message to user in views.py and codestar/base.html

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # class to display all posts on homepage
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "blog/index.html"
     paginate_by = 6
-
 
 def post_detail(request, slug):
     """
@@ -22,11 +23,27 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    comments = post.comments.all().order_by("-created_on")
+    # Fetch only approved comments
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    # Count approved comments
     comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            # Do not approve automatically; leave for admin
+            comment.save()
+            messages.success(
+                request, 
+                'Comment submitted and is awaiting admin approval.'
+            )
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
@@ -35,5 +52,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form
         },
-)
+    )
