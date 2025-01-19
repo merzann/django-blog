8ce47f65f13096e3b1421373136638a158456a2f
Annika Merz <annisch78@gmail.com>
2025-01-19 20:55:22 +0000

    maint: correct validation error for nav links in base.html, add Message Tags to setiings.py to differentiate Django messages

--- codestar/settings.py
+++ codestar/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -133,6 +134,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Message tags to differentiate alert and success messages
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'alert-success',
+    messages.ERROR: 'alert-danger',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
